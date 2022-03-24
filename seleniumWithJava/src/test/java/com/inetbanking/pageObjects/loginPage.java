47a03ce65c0406e3bb322f92ac8e24c0a81d62dd
ShihabSaif <a.b.m.shihab57@gmail.com>
2022-03-24 22:31:34 +0600

    necessary driver exe file has been copied in driver folder

---
+++ seleniumWithJava/src/test/java/com/inetbanking/pageObjects/loginPage.java
@@ -0,0 +1,40 @@
+package com.inetbanking.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class loginPage {
+	WebDriver ldriver;
+	
+	loginPage(WebDriver rdriver)
+	{
+		ldriver = rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+	
+	@FindBy(name="uid")
+	WebElement txtusername;
+	
+	@FindBy(name="password")
+	WebElement txtpassword;
+	
+	@FindBy(name="btnlogin")
+	WebElement btnlogin;
+	
+	public void setUserName(String name)
+	{
+		txtusername.sendKeys(name);
+	}
+	
+	public void setPassword(String password)
+	{
+		txtpassword.sendKeys(password);
+	}
+	
+	public void clickLogin()
+	{
+		btnlogin.click();
+	}
+}
