004cee1cfd75c632c985be70cddcdc21b5bf71f6
ShihabSaif <a.b.m.shihab57@gmail.com>
2022-03-25 20:35:51 +0600

    update in base class

--- seleniumWithJava/src/test/java/com/inetbanking/pageObjects/BaseClass.java
+++ seleniumWithJava/src/test/java/com/inetbanking/pageObjects/BaseClass.java
@@ -1,5 +1,26 @@
 package com.inetbanking.pageObjects;
 
-public class BaseClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
+public class BaseClass {
+	public String baseURL = "https://demo.guru99.com/V1/index.php";
+	public String username = "mngr394861";
+	public String password = "bYbEsuq";
+	public static WebDriver driver;
+	
+	@BeforeClass
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "F:\\Testing Practises\\Selenium using Java\\seleniumWithJava\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+	}
 }
