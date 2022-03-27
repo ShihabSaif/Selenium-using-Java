5d74af4e72ee0db23c5d196628634c40b0621721
ShihabSaif <a.b.m.shihab57@gmail.com>
2022-03-27 14:43:55 +0600

    point no 10 of readme file(adding new testcases) has been done

---
+++ seleniumWithJava/src/test/java/com/inetbanking/testcases/TC_AddCustomerTest_003.java
@@ -0,0 +1,74 @@
+package com.inetbanking.testcases;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.inetbanking.pageObjects.AddCustomerPage;
+import com.inetbanking.pageObjects.BaseClass;
+import com.inetbanking.pageObjects.loginPage;
+
+public class TC_AddCustomerTest_003 extends BaseClass {
+	@Test
+	public void addNewCustomer() throws InterruptedException, IOException
+	{
+		loginPage lp=new loginPage(driver);
+		lp.setUserName(username);
+		logger.info("User name is provided");
+		lp.setPassword(password);
+		logger.info("Passsword is provided");
+		lp.clickLogin();
+		
+		Thread.sleep(3000);
+		
+		AddCustomerPage addcust=new AddCustomerPage(driver);
+		
+		addcust.clickAddNewCustomer();
+		
+		logger.info("providing customer details....");
+		
+		addcust.custName("Pavan");
+		logger.info("customer name provided....");
+		addcust.custgender("male");
+		addcust.custdob("10","15","1985");
+		Thread.sleep(5000);
+		addcust.custaddress("INDIA");
+		addcust.custcity("HYD");
+		addcust.custstate("AP");
+		addcust.custpinno("5000074");
+		addcust.custtelephoneno("987890091");
+		
+		String email=randomestring()+"@gmail.com";
+		addcust.custemailid(email);
+		addcust.custpassword("abcdef");
+		addcust.custsubmit();
+		
+		Thread.sleep(3000);
+		
+		logger.info("validation started....");
+		
+		boolean res=driver.getPageSource().contains("Customer Registered Successfully!!!");
+		
+		if(res==true)
+		{
+			Assert.assertTrue(true);
+			logger.info("test case passed....");
+			
+		}
+		else
+		{
+			logger.info("test case failed....");
+			captureScreen(driver,"addNewCustomer");
+			Assert.assertTrue(false);
+		}
+			
+	}
+	
+	public String randomestring()
+	{
+		String generatedString = RandomStringUtils.randomAlphabetic(8);
+		return generatedString;
+	}
+}
